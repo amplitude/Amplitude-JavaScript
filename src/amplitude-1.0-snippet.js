39ff485601dae2e8352686d90624b6fd8f7d593a
Curtis <curtis@sonalight.com>
2013-01-14 17:16:42 -0800

    v1.0 commit

---
+++ src/amplitude-1.0-snippet.js
@@ -0,0 +1,20 @@
+(function(window, document) {
+  var amplitude = window.amplitude || {};
+  var as = document.createElement('script');
+  as.type = 'text/javascript';
+  as.async = true;
+  as.src = 'http://cdn.amplitude.com/libs/amplitude-1.0-min.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(as, s);  
+  amplitude._q = [];
+  function proxy(fn) {
+    amplitude[fn] = function() {
+      amplitude._q.push([fn].concat(Array.prototype.slice.call(arguments, 0)));
+    };
+  }
+  var funcs = ["init", "logEvent", "setUserId", "setGlobalUserProperties"];
+  for (var i = 0; i < funcs.length; i++) {
+    proxy(funcs[i]);
+  };
+  window.amplitude = amplitude;
+})(window, document);
