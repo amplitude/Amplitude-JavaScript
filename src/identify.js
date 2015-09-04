98ad2e26363f5c47552aa0d3fdfb3945a8e4a2a6
Daniel Jih <daniel@amplitude.com>
2015-09-04 12:09:17 -0700

    adding identify class

---
+++ src/identify.js
@@ -0,0 +1,59 @@
+// var querystring = require('querystring');
+
+/*
+ * Wrapper for a user properties JSON object that supports operations
+ */
+
+var AMP_OP_ADD = '$add';
+var AMP_OP_SET = '$set';
+var AMP_OP_SET_ONCE = '$setOnce';
+var AMP_OP_UNSET = '$unset';
+
+
+var Identify = function() {
+  this.userPropertiesOperations = {};
+  this.properties = []; // keep track of keys that have been added
+};
+
+Identify.prototype.add = function(property, value) {
+  // validate value is number??
+  this._addOperation(AMP_OP_ADD, property, value);
+  return this;
+};
+
+Identify.prototype.set = function(property, value) {
+  this._addOperation(AMP_OP_SET, property, value);
+  return this;
+};
+
+Identify.prototype.setOnce = function(property, value) {
+  this._addOperation(AMP_OP_SET_ONCE, value);
+  return this;
+};
+
+Identify.prototype.unset = function(property) {
+  this._addOperation(AMP_OP_UNSET, property, '-');
+  return this;
+};
+
+Identify.prototype._addOperation = function(operation, property, value) {
+  // check that property wasn't already used in this Identify
+  if (this.properties.indexOf(property) !== -1) {
+    return;
+  }
+
+  if (!(operation in this.userPropertiesOperations)){
+    this.userPropertiesOperations[operation] = {};
+  }
+  this.userPropertiesOperations[operation][property] = value;
+  this.properties.push(property);
+};
+
+
+module.exports = {
+  Identify: Identify,
+  AMP_OP_ADD: AMP_OP_ADD,
+  AMP_OP_SET: AMP_OP_SET,
+  AMP_OP_SET_ONCE: AMP_OP_SET_ONCE,
+  AMP_OP_UNSET: AMP_OP_UNSET
+};
---
+++ test/identify.js
@@ -0,0 +1,59 @@
+var Identify = require('../src/identify.js');
+
+describe('Identify', function() {
+
+  var encodeCases = [
+    ['', ''],
+    ['f', 'Zg=='],
+    ['fo', 'Zm8='],
+    ['foo', 'Zm9v'],
+    ['foob', 'Zm9vYg=='],
+    ['fooba', 'Zm9vYmE='],
+    ['foobar', 'Zm9vYmFy'],
+    ['\u2661', '4pmh']
+  ];
+
+  var decodeCases = [
+    ['', ''],
+    ['Zg', 'f'],
+    ['Zg==', 'f'],
+    ['Zm8', 'fo'],
+    ['Zm8=', 'fo'],
+    ['Zm9v', 'foo'],
+    ['Zm9vYg', 'foob'],
+    ['Zm9vYg==', 'foob'],
+    ['Zm9vYmE', 'fooba'],
+    ['Zm9vYmE=', 'fooba'],
+    ['Zm9vYmFy', 'foobar'],
+    ['4pmh', '\u2661']
+  ];
+
+  it('should unset properties', function () {
+    var property1 = 'testProperty1';
+    var property2 = 'testProperty2';
+    var identify = new Identify.Identify().unset(property1).unset(property2);
+
+    console.log(Identify.AMP_OP_UNSET);
+
+    var expected = {};
+    expected[Identify.AMP_OP_UNSET] = {
+      property1: '-',
+      property2: '-'
+    };
+    assert.equal(expected, identify.userPropertiesOperations);
+  });
+
+/*
+  it('should encode properly', function() {
+    for (var i = 0; i < encodeCases.length; i++) {
+      assert.equal(encodeCases[i][1], Base64.encode(encodeCases[i][0]));
+    }
+  });
+
+  it('should decode properly', function() {
+    for (var i = 0; i < decodeCases.length; i++) {
+      assert.equal(decodeCases[i][1], Base64.decode(decodeCases[i][0]));
+    }
+  });
+*/
+});
