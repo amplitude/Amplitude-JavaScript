6a8bb10f50e43fd120ac1d35c749612ac844e538
Daniel Jih <daniel@amplitude.com>
2015-09-23 21:33:50 -0700

    generalize identify prototype from func dict

--- src/amplitude-snippet.js
+++ src/amplitude-snippet.js
@@ -6,11 +6,16 @@
   as.src = 'https://d24n15hnbwhuhn.cloudfront.net/libs/amplitude-2.4.1-min.gz.js';
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(as, s);
-  amplitude.Identify = function(){this.p={'a':{},'s':{},'u':{},'so':{}};};
-  amplitude.Identify.prototype.add = function(k,v){this.p.a[k]=v;return this;};
-  amplitude.Identify.prototype.set = function(k,v){this.p.s[k]=v;return this;};
-  amplitude.Identify.prototype.setOnce = function(k,v){this.p.so[k]=v;return this;};
-  amplitude.Identify.prototype.unset = function(k){this.p.u[k]='-';return this;};
+  var identifyFuncs = {'add':['a', false], 'set':['s', false], 'setOnce':['so', false], 'unset':['u', true]};
+  amplitude.Identify = function(){ this.p = {}; Object.keys(identifyFuncs).forEach(function (key) {
+    this.p[identifyFuncs[key][0]] = {};
+  }.bind(this));};
+  function proxyIdentifyFunc(fn, dict, overrideValue) {
+    amplitude.Identify.prototype[fn] = function(k,v) { this.p[dict][k]=overrideValue?'-':v; return this; };
+  }
+  Object.keys(identifyFuncs).forEach(function (key) {
+    proxyIdentifyFunc(key, identifyFuncs[key][0], identifyFuncs[key][1]);
+  });
   amplitude._q = [];
   function proxy(fn) {
     amplitude[fn] = function() {
