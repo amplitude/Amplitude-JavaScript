96977ed4a647291b8a5ac317851a2b57b39f18c6
Daniel Jih <daniel@amplitude.com>
2016-03-25 11:46:03 -0700

    release 3.0.0b

--- amplitude.js
+++ amplitude.js
@@ -3978,7 +3978,7 @@ module.exports = uuid;
 
 }, {}],
 8: [function(require, module, exports) {
-module.exports = '2.9.1';
+module.exports = '3.0.0b';
 
 }, {}],
 9: [function(require, module, exports) {
