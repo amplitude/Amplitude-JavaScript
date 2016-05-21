bec424ca6532cd3ef11fbf138c8f8ebfd9e40df9
Daniel Jih <djih@stanford.edu>
2016-05-20 18:50:50 -0700

    updated index to copy all instances, and added snippet tests

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@
 var Amplitude = require('./amplitude');
 
 var old = window.amplitude || {};
-var instance = new Amplitude();
-instance._q = old._q || [];
+var newInstance = new Amplitude();
+newInstance._q = old._q || [];
+for (var instance in old._iq) { // migrate eat instance's queue
+  if (old._iq.hasOwnProperty(instance)) {
+    newInstance.getInstance(instance)._q = old._iq[instance]._q || [];
+  }
+}
 
 // export the instance
-module.exports = instance;
+module.exports = newInstance;
