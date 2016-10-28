5523cab25d76a8b01b17a0639f4fbd52a0440c3d
Daniel Jih <daniel@amplitude.com>
2016-10-28 11:46:19 -0700

    v3.3.2

--- amplitude.js
+++ amplitude.js
@@ -4949,7 +4949,7 @@ module.exports = uuid;
 
 }, {}],
 10: [function(require, module, exports) {
-module.exports = '3.3.1';
+module.exports = '3.3.2';
 
 }, {}],
 11: [function(require, module, exports) {
