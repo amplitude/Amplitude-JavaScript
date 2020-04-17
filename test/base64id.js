ea73235b12cf20275c40f481f4c153a342f6182b
Krishna Rajendran <krishna@emptybox.org>
2020-04-17 14:51:05 -0700

    Use shorter base64 based ids for device IDs

---
+++ test/base64id.js
@@ -0,0 +1,13 @@
+import base64Id from '../src/base64Id';
+
+describe('base64Id', () => {
+  it('should return an id with length 22', () => {
+    assert.equal(base64Id().length, 22);
+  });
+
+  // If this test fails randomly it'll be frustrating to reproduce.  Ideally
+  // there would be some reproducible seed we would print for every test run.
+  it('should return an id of safe base64 characters', () => {
+    assert.equal(true, /^[a-zA-Z0-9\-_]*$/.test(base64Id()))
+  });
+});
