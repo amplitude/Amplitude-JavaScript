e2718b939792b5ed5284b41501fe2574459864e8
Krishna Rajendran <krishna@amplitude.com>
2020-05-01 22:33:05 -0700

    Pass the amplitude instance to onInit (#259)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /* jshint expr:true */
 import Amplitude from  './amplitude';
 
-var old = window.amplitude || {};
-var newInstance = new Amplitude();
+const old = window.amplitude || {};
+const newInstance = new Amplitude();
 newInstance._q = old._q || [];
-for (var instance in old._iq) { // migrate each instance's queue
+for (let instance in old._iq) { // migrate each instance's queue
   if (old._iq.hasOwnProperty(instance)) {
     newInstance.getInstance(instance)._q = old._iq[instance]._q || [];
   }
