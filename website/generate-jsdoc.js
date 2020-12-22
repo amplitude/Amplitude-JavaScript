1dcc59b19068a51ffdc0f2638ee35642bd91ddcd
Kelson Warner <kelson.warner@amplitude.com>
2020-12-21 17:06:54 -0800

    build: added website directory to prettier and eslint checks

--- website/generate-jsdoc.js
+++ website/generate-jsdoc.js
@@ -1,55 +1,37 @@
-const jsdoc2md = require("jsdoc-to-markdown");
-const fs = require("fs");
-const path = require("path");
-const prettier = require("prettier");
-const publicClassFiles = [
-  "amplitude-client.js",
-  "amplitude.js",
-  "identify.js",
-  "revenue.js",
-];
-const publicTypedefFiles = ["options.js"];
-const srcDir = path.join(__dirname, "../", "src");
-const outputDir = path.join(__dirname, "docs");
+const jsdoc2md = require('jsdoc-to-markdown');
+const fs = require('fs');
+const path = require('path');
+const prettier = require('prettier');
+const publicClassFiles = ['amplitude-client.js', 'amplitude.js', 'identify.js', 'revenue.js'];
+const publicTypedefFiles = ['options.js'];
+const srcDir = path.join(__dirname, '../', 'src');
+const outputDir = path.join(__dirname, 'docs');
 
 function generateTypedefMarkdown(inputFile) {
   const inputFilePath = path.join(srcDir, inputFile);
   const data = jsdoc2md.getTemplateDataSync({ files: inputFilePath });
-  const name = data.find((e) => e.kind === "typedef").name;
-  const filteredData = data.filter((e) => e.kind === "typedef");
-  const outputFilePath = path.join(outputDir, `${name}.md`);
-  const markdownOutput = filteredData
-    .map((item) => documentOptionsFile(item))
-    .join("\n");
-  fs.writeFileSync(
-    path.join(outputDir, `${name}.md`),
-    prettier.format(markdownOutput, { parser: "mdx" })
-  );
+  const name = data.find((e) => e.kind === 'typedef').name;
+  const filteredData = data.filter((e) => e.kind === 'typedef');
+
+  const markdownOutput = filteredData.map((item) => documentOptionsFile(item)).join('\n');
+  fs.writeFileSync(path.join(outputDir, `${name}.md`), prettier.format(markdownOutput, { parser: 'mdx' }));
 }
 
 function generateClassMarkdown(inputFile) {
   const inputFilePath = path.join(srcDir, inputFile);
   const data = jsdoc2md.getTemplateDataSync({ files: inputFilePath });
-  const className = data.find((e) => e.kind === "class").name;
+  const className = data.find((e) => e.kind === 'class').name;
   const filteredData = data.filter(
-    (e) =>
-      e.kind === "constructor" ||
-      (e.access === "public" && (e.kind === "function" || e.kind === "member"))
+    (e) => e.kind === 'constructor' || (e.access === 'public' && (e.kind === 'function' || e.kind === 'member')),
   );
-  const outputFilePath = path.join(outputDir, `${className}.md`);
 
-  const markdownOutput = filteredData
-    .map((item) => documentClassFile(item))
-    .join("\n");
-  fs.writeFileSync(
-    path.join(outputDir, `${className}.md`),
-    prettier.format(markdownOutput, { parser: "mdx" })
-  );
+  const markdownOutput = filteredData.map((item) => documentClassFile(item)).join('\n');
+  fs.writeFileSync(path.join(outputDir, `${className}.md`), prettier.format(markdownOutput, { parser: 'mdx' }));
 }
 
 function documentOptionsFile(data) {
   return `${documentHeader(data)}
-  
+
   ${data.description}
 
 Option | Type | Description |	Default
@@ -63,25 +45,25 @@ function documentOptionsProperties(data) {
   return data.properties
     .map(
       (prop) =>
-        `${prop.name || ""} | ${data.properties[0].type.names.join("|")} | ${
-          prop.defaultvalue || ""
-        } | ${prop.description || ""}`
+        `${prop.name || ''} | ${data.properties[0].type.names.join('|')} | ${prop.defaultvalue || ''} | ${
+          prop.description || ''
+        }`,
     )
-    .join("\n");
+    .join('\n');
 }
 
 function documentClassFile(data) {
   return `${documentHeader(data)}
 
-${data.examples ? documentExamples(data) : ""}
+${data.examples ? documentExamples(data) : ''}
 
-${data.description || ""} 
+${data.description || ''}
 
-${data.deprecated ? documentDeprecated(data) : ""}
+${data.deprecated ? documentDeprecated(data) : ''}
 
-${data.params ? documentParams(data) : ""}
+${data.params ? documentParams(data) : ''}
 
-${data.returns ? documentReturn(data) : ""}
+${data.returns ? documentReturn(data) : ''}
 `;
 }
 
@@ -106,18 +88,18 @@ function documentDeprecated(data) {
 
 function documentParams(data) {
   const params = data.params.map(
-    (param) => `- \`${param.name}\` (\`${param.type.names.join("|")}\`)
+    (param) => `- \`${param.name}\` (\`${param.type.names.join('|')}\`)
 ${param.description}
-`
+`,
   );
   return `### Parameters
-${params.join("\n")}
+${params.join('\n')}
 `;
 }
 
 function documentReturn(data) {
   return `### Return Value
-- (\`${data.returns[0].type.names.join("|")}\`)
+- (\`${data.returns[0].type.names.join('|')}\`)
 ${data.returns[0].description}
 `;
 }
@@ -135,4 +117,3 @@ for (const file of publicClassFiles) {
 for (const file of publicTypedefFiles) {
   generateTypedefMarkdown(file);
 }
-
