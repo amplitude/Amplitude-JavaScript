8715ba03903e2608a76ca1d4ac927901c717cfc6
John Tran <jptran318@gmail.com>
2021-11-16 15:13:52 -0800

    nits

--- scripts/create-snippet-instructions.js
+++ scripts/create-snippet-instructions.js
@@ -1,18 +1,17 @@
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-// Update the README with the minified snippet.
-var cwd = process.cwd();
-var instructionsFileName = path.join(cwd, 'amplitude-snippet-instructions.js');
+const cwd = process.cwd();
+const instructionsFileName = path.join(cwd, 'amplitude-snippet-instructions.js');
 
-var snippetFilename = path.join(cwd, 'amplitude-snippet.min.js');
-var snippet = fs.readFileSync(snippetFilename, 'utf-8');
+const snippetFilename = path.join(cwd, 'amplitude-snippet.min.js');
+const snippet = fs.readFileSync(snippetFilename, 'utf-8');
 
-var script = `<script type="text/javascript">
+const script = `<script type="text/javascript">
 ${snippet}
 amplitude.getInstance().init("YOUR_API_KEY_HERE")
 </script>
 `;
 fs.writeFileSync(instructionsFileName, script);
 
-console.log('Updated amplitude-snippet-instructions.js');
+console.log('Created amplitude-snippet-instructions.js');
