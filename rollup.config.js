36ddf9d3fb07d2db2cf002582cf79544f5793323
Brian Giori <brian.giori@amplitude.com>
2021-12-03 11:34:47 -0800

    build, tests fail

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import replace from 'rollup-plugin-replace';
-import babel from 'rollup-plugin-babel';
-import json from 'rollup-plugin-json';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 
 export default {
   input: 'src/index.js',
@@ -14,20 +14,26 @@ export default {
       id: 'amplitude',
     }
   },
+  external: [/@babel\/runtime/],
   plugins: [
     json(),
-    babel({
-      exclude: 'node_modules/**',
-      plugins: ['@babel/plugin-proposal-object-rest-spread'],
-    }),
     resolve({
       browser: true,
     }),
     replace({
+      preventAssignment: false,
       BUILD_COMPAT_SNIPPET: 'true',
       BUILD_COMPAT_LOCAL_STORAGE: 'true',
       BUILD_COMPAT_2_0: 'true',
     }),
     commonjs(),
+    babel({
+      babelHelpers: 'runtime',
+      exclude: 'node_modules/**',
+      plugins: [
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-transform-runtime'
+      ],
+    }),
   ],
 };
--- rollup.esm.js
+++ rollup.esm.js
@@ -1,7 +1,7 @@
-import commonjs from 'rollup-plugin-commonjs';
-import replace from 'rollup-plugin-replace';
-import babel from 'rollup-plugin-babel';
-import json from 'rollup-plugin-json';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import babel from '@rollup/plugin-babel';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/index.js',
@@ -10,23 +10,25 @@ export default {
     file: 'amplitude.esm.js',
     format: 'esm',
   },
+  external: [/@babel\/runtime/],
   plugins: [
     json(),
-    babel({
-      exclude: 'node_modules/**',
-      plugins: [
-        '@babel/plugin-transform-runtime',
-        '@babel/plugin-proposal-object-rest-spread'
-      ],
-      runtimeHelpers: true
-    }),
     replace({
+      preventAssignment: false,
       BUILD_COMPAT_SNIPPET: 'false',
       BUILD_COMPAT_LOCAL_STORAGE: 'true',
       BUILD_COMPAT_2_0: 'true',
     }),
     commonjs({
       include: "node_modules/**"
+    }),
+    babel({
+      babelHelpers: 'runtime',
+      exclude: 'node_modules/**',
+      plugins: [
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-transform-runtime'
+      ],
     }),
   ],
 };
