2440e9a0309236a27bd639ced87b2d2187d2d48b
Dean Shi <dnshi@users.noreply.github.com>
2022-05-10 22:18:27 -0700

    fix: assign domain after cookie storage options are given (#528)
    * fix: Assign domain after cookie storage options are added
    
    * fix: address comment

--- src/amplitude-client.js
+++ src/amplitude-client.js
@@ -141,12 +141,13 @@ AmplitudeClient.prototype.init = function init(apiKey, opt_userId, opt_config, o
     const hasNewCookie = !!this._metadataStorage.load();
     this._useOldCookie = !hasNewCookie && hasOldCookie && !this.options.cookieForceUpgrade;
     const hasCookie = hasNewCookie || hasOldCookie;
-    this.options.domain = this.cookieStorage.options().domain;
 
     if (this.options.deferInitialization && !hasCookie) {
       this._deferInitialization(apiKey, opt_userId, opt_config, opt_callback);
       return;
     }
+
+    this.options.domain = this.cookieStorage.options().domain;
 
     if (type(this.options.logLevel) === 'string') {
       utils.setLogLevel(this.options.logLevel);
